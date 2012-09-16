78c5c0ada3df099ae5a9f54761189a5575f935fc
Andy <andy@woah.(none)>
2012-09-16 16:41:52 -0400

    Created new rails application

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeSwap::Application.config.secret_token = '52843899d324fede2f3976804d141c635bbe7ade9a0001c86173556472ca00a6ec51e6c35d1b74d763fdec83be8b37da4a2b4b6e01e91192ac3ff3f996975145'
