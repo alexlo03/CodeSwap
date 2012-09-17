0d5b0b7c5cf179165bd4cd2763b9d34a90d0c689
michaeaj <andmichaelis@gmail.com>
2012-09-17 18:23:52 -0400

    Added user authorization

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  ROLES = %w[admin faculty ta student]
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,5 +9,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
   # attr_accessible :title, :body
 end
