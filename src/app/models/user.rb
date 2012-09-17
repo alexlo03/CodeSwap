284bda8b760756e217d8a2ea1c1923461cca9533
michaeaj <andmichaelis@gmail.com>
2012-09-17 19:14:38 -0400

    Added more things related to user authentication! Woo!

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -8,7 +8,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
 
+  
   # attr_accessible :title, :body
+
+  def admin?
+    role.eql? "admin"
+  end
+
+  def faculty?
+    role.eql? "faculty"
+  end
+
+  def ta?
+    role.eql? "ta"
+  end
+
+  def student?
+    role.eql? "student"
+  end
+
 end
