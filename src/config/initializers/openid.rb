2496f2228b03f9c1b782c16c76702f6fd00c4969
Jackson <jackson@jackson-HP-EliteBook-8530w.(none)>
2012-09-30 15:31:20 -0400

    Fixed bug(?) where users could add several of the same type of account to authenticate with

--- src/config/initializers/openid.rb
+++ src/config/initializers/openid.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'LSND7ekG525w7OIsLYaPSQ', 'plZaDzNATAFfZlfyhyU3c5XBn3dtUZFV3vRQmmUKc'
-  # provider :google, '227068049875.apps.googleusercontent.com', 'NIj3MuESOhlVKND79W5xOPvs'
+#  provider :google, 'localhost',  'NIj3MuESOhlVKND79W5xOPvs'
 end
