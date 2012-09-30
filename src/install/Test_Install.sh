f2c6855044a1a77041bd1b106cb72360083570b8
Caleb Drake <drakecb@rose-hulman.edu>
2012-09-30 17:58:10 -0400

    source change for RVM was not occurring, moved command after dependencies are installed

--- src/install/Test_Install.sh
+++ src/install/Test_Install.sh
@@ -4,11 +4,14 @@ sudo apt-get update  # To get the latest package lists
 sudo apt-get install curl
 curl -L get.rvm.io | bash -s stable
 
-#Install and run rvm
-source ~/.rvm/scripts/rvm
+
 
 #Acquire dependencies for rvm
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+
+#Install and run rvm
+source ~/.rvm/scripts/rvm
+
 #Use rvm to install ruby version 1.9.3
 rvm install 1.9.3
 
