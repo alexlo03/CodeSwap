dbe91faccec3abf451665931627c3236c6420f0e
Jackson <jackson@jackson-HP-EliteBook-8530w.(none)>
2012-10-16 12:10:24 -0400

    added basic loading panel

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -24,8 +24,8 @@ CodeSwap::Application.configure do
     domain:  "rose-hulman.edu",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "rosehulman.codeswap",
+    password: "hackmenot"
   }
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
