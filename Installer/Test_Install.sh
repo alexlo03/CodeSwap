808a7a26978a303f28ea2e4c182d557f92ce4886
Caleb Drake <drakecb@rose-hulman.edu>
2012-10-30 16:59:36 -0400

    added *command* beform rvm & rake commands

--- Installer/Test_Install.sh
+++ Installer/Test_Install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 echo "\033[01;32mUpdating...\033[00m\n"
-sudo apt-get update -y -qq # To get the latest package lists
+sudo apt-get update -y # To get the latest package lists
 
 #Unpacks curl archive
 
@@ -20,26 +20,26 @@ sudo make install -s
 
 
 echo "\033[01;32mAcquiring dependencies for rvm\033[00m\n"
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev libruby1.9.1 curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion -y -qq
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev libruby1.9.1 curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion -y
 
 echo "\033[01;32mInstalling and Running rvm\033[00m\n"
 command curl -L https://get.rvm.io  | bash -s stable --ruby
 #curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash
 
 echo "\033[01;32mReloading RVM\033[00m\n"
-rvm reload
+command rvm reload
 
 echo "\033mUsing rvm to install ruby version 1.9.3\033[00m\n"
 #command rvm install 1.9.3
 
 echo "\033[01;32mMaking rvm use 1.9.3 as the default version\033[00m\n"
-rvm use 1.9.3 --default
+command rvm use 1.9.3 --default
 
 echo "\033[01;32mUpdating rubygems with rvm\033[00m\n"
-rvm rubygems current
+command rvm rubygems current
 
 echo "\033[01;32mInstalling rails\033[00m\n"
-gem install rails
+command gem install rails
 
 echo "\033[01;32mDownloading & Installing node.js\033[00m\n"
 git clone https://github.com/joyent/node.git
@@ -61,10 +61,7 @@ echo "\033[01;32mInstalling/Updating bundle\033[00m\n"
 bundle install
 
 echo "\033[01;32mSetting-up the database\033[00m\n"
-rake db:migrate
-rake db:reset
-
-echo "\033[01;32mStarting the server\033[00m\n"
-rails server
+command rake db:migrate
+command rake db:reset
 
 echo "\033[01;33mWOOHOO\x21  The system is set-up and ready to go\x21\033[00m\n"
