eb5331b1b3b9c53ee873115eb6378267c70f698b
Andrew Michaelis <andmichaelis@gmail.com>
2012-11-07 13:42:05 -0500

    Added .pdf of deployment plan

--- src/script/startserver.sh
+++ src/script/startserver.sh
@@ -1,3 +1,3 @@
 sudo screen -S server -d -m rails s -p 80
 
-sudo screen -S secureserver -d -m bundle exec ruby secure_rails s -p 443
+sudo screen -S secureserver -d -m bundle exec ruby secure_rails s -p 443  
