7f76b4ed2aef757d4d39f01a5bfac0108a41bf8a
Jackson <jackson@jackson.(none)>
2013-01-28 20:39:46 -0500

    typo fix

--- src/app/models/review_question.rb
+++ src/app/models/review_question.rb
@@ -1,5 +1,5 @@
 class ReviewQuestion < ActiveRecord::Base
-	belongs_to: :review_assignment
+	belongs_to :review_assignment
   attr_accessible :content, :type, :review_assignment_id
 	
 	#for the different types
@@ -9,7 +9,7 @@ class ReviewQuestion < ActiveRecord::Base
 	TYPES = %w{ multipleChoice numerical shortAnswer }
 
 	TYPES.each_with_index do |name,i|
-		define_method("#{name}") {type == i}
+		define_method("#{name}?") {type == i}
 	end
 
 	
