b92ca1ac10ab7f9a54169bdd96a9c6972ddd73b1
Caleb Drake <calebbdrake@gmail.com>
2013-05-13 11:30:47 -0400

    added migration defaulting User.role to student

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424045312) do
+ActiveRecord::Schema.define(:version => 20130507221409) do
 
   create_table "assignment_definition_to_users", :force => true do |t|
     t.integer  "user_id"
@@ -170,8 +170,8 @@ ActiveRecord::Schema.define(:version => 20130424045312) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",        :null => false
+    t.string   "encrypted_password",     :default => "",        :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -180,9 +180,9 @@ ActiveRecord::Schema.define(:version => 20130424045312) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "role"
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.string   "role",                   :default => "student"
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "deleted_at"
